16dd2384996083c8debde836c13453b2129cd002
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-08-12 10:57:42 +0600

    honor page

---
+++ app/honor/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center'>
+      <h1>At this moment no memorial build yet. In near future if the memorial build we will add.</h1>
+    </div>
+  )
+}
+
+export default page
