f0fefaf765c69e5962c7c121e2a7468b9c7d7229
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-08-12 11:08:45 +0600

    asdasda

--- app/honor/page.jsx
+++ app/honor/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center text-3xl py-28'>
       <h1>At this moment no memorial build yet. In near future if the memorial build we will add.</h1>
     </div>
   )
